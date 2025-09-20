83d863ce78577d511648a8e473476298d046148e
Mig3ul <mianrobra2602@gmail.com>
2025-09-20 11:42:33 -0500

    :v

---
+++ src/main/java/com/mycompany/proyectojava/Proyectil.java
@@ -0,0 +1,9 @@
+package com.mycompany.proyectojava;
+
+public class Proyectil {
+    protected double masa;
+    protected double velocidadInicial;
+    protected double velocidadInicialX;
+    protected double velocidadInicialY;
+    protected double velocidadInicialZ;
+}
