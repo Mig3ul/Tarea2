262903e1a637f3ac3d2c1eaa8a102dc79572d52f
Mig3ul <mianrobra2602@gmail.com>
2025-09-22 09:36:45 -0500

    constructor añadido, metodo del calculo de velocidades iniciales y metodos obligatorios para subclases tambien añadidos

--- src/main/java/com/mycompany/proyectil/Proyectil.java
+++ src/main/java/com/mycompany/proyectil/Proyectil.java
@@ -2,32 +2,41 @@ package com.mycompany.proyectil;
 
 public abstract class Proyectil {
 
-        protected double masa;
-        protected double velocidadInicial; //v0
-        protected double v0X, v0Y,v0Z;
-        protected double[] posX, pozY, posZ;
-        protected double anguloTheta, anguloPhi; //angulo Theta y angulo Phi
-        protected double cosenoTheta , cosenoPhi;
-        protected double senoTheta, senoPhi;
+    protected double masa;
+    protected double v0; //velocidad inicial
+    protected double anguloTheta, anguloPhi;
+    protected double cosTheta , cosPhi;
+    protected double sinTheta, sinPhi;
 
-        protected static final double GRAVEDAD = 9.8; //9.8m/s^2 
+    protected double v0X, v0Y,v0Z; // Componentes de la velocidad
+    protected double[] x0, y0, z0; //posiciones iniciales
 
-        public Proyectil() {
-            this.masa = 0.0;
-            this.velocidadInicial = 0.0;
-            v0X = 0.0;
-            v0Y = 0.0;
-            v0Z = 0.0;
+    protected static final double GRAVEDAD = 9.8; //9.8m/s^2 
 
-            this.anguloTheta = Math.toRadians(anguloTheta); //Angulo Theta en radianes
-            this.anguloPhi = Math.toRadians(anguloPhi); // Angulo phi en radianes
-            this.cosenoTheta = Math.cos(anguloTheta); // coseno de theta 
-            this.cosenoPhi = Math.cos(anguloPhi); // coseno de phi
+    public Proyectil() {
+        this.masa = 0.0;
+        this.v0 = 0.0;
 
-            this.senoTheta = Math.sin(anguloTheta);
-            this.senoPhi = Math.sin(anguloPhi);
-        }
+        this.anguloTheta = Math.toRadians(anguloTheta); //Angulo theta en radianes
+        this.anguloPhi = Math.toRadians(anguloPhi); // Angulo phi en radianes
+        this.cosTheta = Math.cos(anguloTheta); // coseno de theta 
+        this.cosPhi = Math.cos(anguloPhi); // coseno de phi
 
-        
+        this.sinTheta = Math.sin(anguloTheta); // seno de Theta
+        this.sinPhi = Math.sin(anguloPhi); // seno de phi
 
+        this.v0X = 0.0;
+        this.v0Y = 0.0;
+        this.v0Z = 0.0;
+    }
+
+    public void calculoVelocidadesIniciales(){
+        v0X = v0 * cosTheta * cosPhi;
+        v0Y = v0 * cosTheta * sinPhi;
+        v0Z = v0 * sinTheta;
+    }
+
+    public abstract double calcularPosicionX(double t);
+    public abstract double calcularPosicionY(double t);
+    public abstract double calcularPosicionZ(double t);
 }
