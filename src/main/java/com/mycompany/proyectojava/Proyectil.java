1db118bedc4d8b6d75fff074e1b7ae492aff1dbb
Mig3ul <mianrobra2602@gmail.com>
2025-09-21 19:14:50 -0500

    se añadieron algunos variables y el constructor

--- src/main/java/com/mycompany/proyectojava/Proyectil.java
+++ src/main/java/com/mycompany/proyectojava/Proyectil.java
@@ -1,9 +1,33 @@
 package com.mycompany.proyectojava;
 
-public class Proyectil {
-    protected double masa;
-    protected double velocidadInicial;
-    protected double velocidadInicialX;
-    protected double velocidadInicialY;
-    protected double velocidadInicialZ;
+public abstract class Proyectil {
+
+        protected double masa;
+        protected double velocidadInicial; //v0
+        protected double v0X, v0Y,v0Z;
+        protected double[] posX, pozY, posZ;
+        protected double anguloTheta, anguloPhi; //angulo Theta y angulo Phi
+        protected double cosenoTheta , cosenoPhi;
+        protected double senoTheta, senoPhi;
+
+        protected static final double GRAVEDAD = 9.8; //9.8m/s^2 
+
+        public Proyectil() {
+            this.masa = 0.0;
+            this.velocidadInicial = 0.0;
+            v0X = 0.0;
+            v0Y = 0.0;
+            v0Z = 0.0;
+
+            this.anguloTheta = Math.toRadians(anguloTheta); //Angulo Theta en radianes
+            this.anguloPhi = Math.toRadians(anguloPhi); // Angulo phi en radianes
+            this.cosenoTheta = Math.cos(anguloTheta); // coseno de theta 
+            this.cosenoPhi = Math.cos(anguloPhi); // coseno de phi
+
+            this.senoTheta = Math.sin(anguloTheta);
+            this.senoPhi = Math.sin(anguloPhi);
+        }
+
+        
+
 }
