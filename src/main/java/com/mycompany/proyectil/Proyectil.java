01eb5de77423b62d6944c8cb8b0413e84624af8c
Mig3ul <mianrobra2602@gmail.com>
2025-09-22 18:22:14 -0500

    clase Proyectil <<terminada>>

--- src/main/java/com/mycompany/proyectil/Proyectil.java
+++ src/main/java/com/mycompany/proyectil/Proyectil.java
@@ -30,6 +30,24 @@ public abstract class Proyectil {
         this.v0Z = 0.0;
     }
 
+
+    public double getMasa() { return masa; }
+    public double getV0() { return v0; }
+    public double getAnguloTheta() { return Math.toDegrees(anguloTheta); }
+    public double getAnguloPhi() { return Math.toDegrees(anguloPhi); }
+    public double getCosTheta() { return cosTheta; }
+    public double getCosPhi() { return cosPhi; }
+    public double getSinTheta() { return sinTheta; }
+    public double getSinPhi() { return sinPhi; }
+    public double getV0X() { return v0X; }
+    public double getV0Y() { return v0Y; }
+    public double getV0Z() { return v0Z; }
+    public double[] getX0() { return x0; }
+    public double[] getY0() { return y0; }
+    public double[] getZ0() { return z0; }
+    public static double getGravedad() { return GRAVEDAD; }
+
+
     public void calculoVelocidadesIniciales(){
         v0X = v0 * cosTheta * cosPhi;
         v0Y = v0 * cosTheta * sinPhi;
@@ -39,4 +57,16 @@ public abstract class Proyectil {
     public abstract double calcularPosicionX(double t);
     public abstract double calcularPosicionY(double t);
     public abstract double calcularPosicionZ(double t);
+
+
+    public void calcularTrayectoria(double[] tiempos, double[] posX, double[] posY, double [] posZ){
+        for (int i = 0; i < tiempos.length; i++){
+            posX[i] = calcularPosicionX(tiempos[i]);
+            posY[i] = calcularPosicionY(tiempos[i]);
+            posZ[i] = calcularPosicionZ(tiempos[i]);
+        }
+    }
+
+
+
 }
